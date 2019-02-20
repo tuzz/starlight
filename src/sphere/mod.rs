38df30711b3322c6a3e55f3a78484a5e1c45fe67
Chris Patuzzo <chris@patuzzo.co.uk>
2019-02-20 14:36:15 +0000

    Add a method to get a ray-sphere interaction

--- src/sphere/mod.rs
+++ src/sphere/mod.rs
@@ -1,14 +1,46 @@
 use crate::vector::Vector;
+use crate::ray::Ray;
+use crate::interaction::Interaction;
 
-struct Sphere {
-    origin: Vector,
-    radius: f64,
-    color: Vector,
+pub struct Sphere {
+    pub origin: Vector,
+    pub radius: f64,
+    pub color: Vector,
 }
 
 impl Sphere {
-    fn new(origin: Vector, radius: f64, color: Vector) -> Self {
+    pub fn new(origin: Vector, radius: f64, color: Vector) -> Self {
         Self { origin, radius, color }
+    }
+
+    pub fn intersection(&self, ray: Ray) -> Option<Interaction> {
+        // Calculate the vector from the center of the sphere to the ray's origin.
+        let center_to_ray = ray.origin - self.origin;
+
+        // Calculate the coefficients of the quadratic rearrangement:
+        let a = ray.direction.dot(ray.direction);
+        let b = 2.0 * center_to_ray.dot(ray.direction);
+        let c = center_to_ray.dot(center_to_ray) - self.radius * self.radius;
+
+        // Calculate the term under the square root of the quadratic equation:
+        let discriminant = b * b - 4.0 * a * c;
+
+        // If the discriminant is negative, there are no intersections:
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrt = discriminant.sqrt();
+
+        // Find both solutions of the quadratic equation:
+        let t0 = (-b - sqrt) / (a * 2.0);
+        let t1 = (-b + sqrt) / (a * 2.0);
+
+        // The intersection is the smallest positive solution:
+        let ray_t = if t0 > 0.0 { t0 } else { t1 };
+        let normal = ray.at(ray_t) - self.origin;
+
+        Some(Interaction::new(ray_t, normal))
     }
 }
 
--- src/sphere/test.rs
+++ src/sphere/test.rs
@@ -17,3 +17,32 @@ mod new {
         assert_eq!(subject.color, purple);
     }
 }
+
+mod intersection {
+    use super::*;
+
+    fn intersection(ray_y_direction: f64) -> Option<Interaction> {
+        let origin = Vector::new(2.0, 0.0, 0.0);
+        let sphere = Subject::new(origin, 1.0, Vector::default());
+
+        let origin = Vector::new(0.0, 0.0, 0.0);
+        let direction = Vector::new(1.0, ray_y_direction, 0.0);
+        let ray = Ray::new(origin, direction);
+
+        sphere.intersection(ray)
+    }
+
+    #[test]
+    fn it_returns_none_if_the_ray_does_not_intersect_the_sphere() {
+        assert_eq!(intersection(0.6), None);
+    }
+
+    #[test]
+    fn it_returns_the_nearest_interaction_if_the_ray_intersects_the_sphere() {
+        let interaction = intersection(0.5)
+            .expect("The ray should have intersected the sphere.");
+
+        assert_eq!(interaction.ray_t, 1.2);
+        assert_eq!(interaction.normal, Vector::new(-0.8, 0.6, 0.0));
+    }
+}
