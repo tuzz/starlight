3bd4387522fde02b14c4b5dae149b2060525d550
Chris Patuzzo <chris@patuzzo.co.uk>
2019-02-20 14:43:34 +0000

    Add a sphere and trace rays that intersect with it
    At the moment, we don’t have any lights, so I’m
    shading the sphere based on the ray’s distance
    from the intersection with the sphere.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod image;
 mod film;
 mod camera;
 mod sphere;
+mod interaction;
 
 use vector::Vector;
 use image::Image;
 use film::Film;
 use camera::Camera;
+use sphere::Sphere;
 
 fn main() {
     let origin = Vector::new(0.0, 0.0, 0.0);
@@ -18,9 +20,14 @@ fn main() {
     let film = Film::new(8.0, 6.0, image);
     let mut camera = Camera::new(origin, direction, orientation, film);
     let purple = Vector::new(0.5, 0.0, 0.5);
+    let sphere = Sphere::new(Vector::new(0.0, 0.0, 5.0), 4.0, purple);
 
-    camera.trace_rays(|_ray| {
-        purple
+    camera.trace_rays(|ray| {
+        if let Some(interaction) = sphere.intersection(ray) {
+            return sphere.color * interaction.ray_t;
+        }
+
+        Vector::new(0.2, 0.2, 0.2)
     });
 
     camera.take_photograph("hello.png");
