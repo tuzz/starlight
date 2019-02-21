c0ec81cf1fda32878596c80a4c566e21f09a3d51
Chris Patuzzo <chris@patuzzo.co.uk>
2019-02-21 10:52:44 +0000

    Add the sample_li method to light
    This is the name used in the PBRT book for the
    method that takes a surface interaction and
    returns how much radiance falls on that point from
    the light. It’s also used to construct the shadow
    ray which is why it also builds a vector.

--- src/light/test.rs
+++ src/light/test.rs
@@ -16,3 +16,33 @@ mod new {
         assert_eq!(subject.power, power);
     }
 }
+
+mod sample_li {
+    use super::*;
+
+    #[test]
+    fn it_calculates_radiance_by_dividing_the_lights_power_by_the_radius_squared() {
+        let origin = Vector::new(1.0, 2.0, 3.0);
+        let subject = Subject::new(origin, 5.0);
+
+        let origin = Vector::new(4.0, 5.0, 6.0);
+        let interaction = Interaction::new(0.0, origin, Vector::default());
+
+        let (radiance, _) = subject.sample_li(&interaction);
+
+        assert_eq!(radiance, 5.0 / 27.0);
+    }
+
+    #[test]
+    fn it_builds_a_vector_pointing_towards_the_light() {
+        let origin = Vector::new(1.0, 2.0, 3.0);
+        let subject = Subject::new(origin, 5.0);
+
+        let origin = Vector::new(4.0, 5.0, 6.0);
+        let interaction = Interaction::new(0.0, origin, Vector::default());
+
+        let (_, incident) = subject.sample_li(&interaction);
+
+        assert_eq!(incident, Vector::new(-3.0, -3.0, -3.0).normalize());
+    }
+}
